2019a6568d6cc88e3b1e3943a8a14cabb42a197f
Michal Obrocnik <mobrocnik@marex.com>
2024-10-17 09:41:31 +0100

    Hotfix types

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
-import Plot from "react-plotly.js";
-import figure from "../figure.json";
+import Plot, { PlotParams } from "react-plotly.js";
+import figureJson from "../figure.json";
 import "./App.css";
 
-function App() {
-  return (
-    <>
-      <Plot {...figure} />
-    </>
-  );
-}
+const figure = figureJson as unknown as PlotParams;
 
-export default App;
+export function App() {
+  return <Plot {...figure} />;
+}
